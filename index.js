4b568e1e6176f71ae0ab31740254d8603709e8d6
Luke Grayland <lgrayland96@gmail.com>
2022-02-25 20:17:35 +0000

    completed start, stop, and end functionality to all pads

--- index.js
+++ index.js
@@ -1,5 +1,62 @@
-document.getElementById('button').addEventListener('click' ,() => {
+//Global variables
 
-    console.log('play')
+let sampleArray = [
+    {sample : new Pizzicato.Sound('samples/moodyKick.wav')},
+    {sample : new Pizzicato.Sound('samples/shakerLoop1.wav')},
+    {sample : new Pizzicato.Sound('samples/dustyLoop.wav')},
+    {sample : new Pizzicato.Sound('samples/dustyLoop.wav')},
+    {sample : new Pizzicato.Sound('samples/shakerLoop1.wav')},
+    {sample : new Pizzicato.Sound('samples/dustyLoop.wav')},
+    {sample : new Pizzicato.Sound('samples/dustyLoop.wav')},
+    {sample : new Pizzicato.Sound('samples/shakerLoop1.wav')},
+    {sample : new Pizzicato.Sound('samples/dustyLoop.wav')},
+    {sample : new Pizzicato.Sound('samples/dustyLoop.wav')},
+    {sample : new Pizzicato.Sound('samples/shakerLoop1.wav')},
+    {sample : new Pizzicato.Sound('samples/dustyLoop.wav')},
+]
 
-})
+let allCounters = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+let vol = document.getElementById('masterVolume')
+let allPads = document.getElementsByClassName('sampleButton')
+
+//Functions
+
+
+// Event listeners
+
+for (let i=0; i<12; i++){
+
+    allPads.item(i).addEventListener('click', () => {
+        if (allCounters[i] === 0) {
+            allCounters[i] = 1
+            sampleArray[i].sample.play()
+            console.log('Sample ' + (i+1) + ' Playing')
+            allPads[i].style.backgroundColor = 'var(--padRed)'
+
+        } else {
+            allCounters[i] = 0
+            sampleArray[i].sample.stop()
+            console.log('Sample ' + (i+1) + ' Stopped')
+            allPads[i].style.backgroundColor = 'lightgray'
+        }
+    })
+
+    sampleArray[i].sample.on('end', () => {
+        if (allCounters[i] === 1) {
+            allCounters[i] = 0
+            console.log('Sample ' + (i+1) + ' Ended')
+            allPads[i].style.backgroundColor = 'lightgray'
+        }
+    })
+
+    vol.addEventListener('click', () => {
+        let scaledVol = vol.value/100
+        sampleArray[i].sample.volume = scaledVol
+        console.log('Master Volume: ' + scaledVol)
+    })
+}
+
+
+
+
